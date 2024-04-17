5ea02fa1619d8ed1fa835f63434574551a962eac
GytenisBorusas <gytenis.borusas@gmail.com>
2024-04-17 11:32:05 -0600

    setup test

---
+++ index.php
@@ -0,0 +1,65 @@
+~~<?php
+    echo "I love pizza";
+    echo "second love pizza";
+    echo "second love pizza";
+    // this is a comment
+    /* multi line
+    comment
+    */
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Order pizza</button>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
