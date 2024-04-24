4c3a95e5277d28412ab81733877a275335c84559
GytenisBorusas <gytenis.borusas@gmail.com>
2024-04-24 17:03:25 -0600

    check in

--- index.php
+++ index.php
@@ -1,26 +1,37 @@
-~~<?php
-    echo "I love pizza";
-    echo "second love pizza";
-    echo "second love pizza";
-    // this is a comment
-    /* multi line
-    comment
-    */
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    <br>
-    <button>Order pizza</button>
+    <form action="index.php" method="post">
+        <label>quantity:</label><br>
+        <input type="text" name="quantity"><br>
+        <input type="submit" value="total">
+    </form>
 </body>
+
 </html>
+
+
+<?php
+    $item = "pizza";
+    $price = 5.99;
+    $quantity = $_POST["quantity"];
+    $total = null;
+
+    $total = $quantity * $price;
+
+    echo "You have ordered {$quantity} x {$item}(s) <br>"; 
+
+
+    
+    echo "Your total is: \${$total}"
+?>
 
 
 
